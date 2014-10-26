d23a68c1897617fe304d816f025044768d242fb6
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2014-10-26 23:10:44 +0900

    Fix Closure.hs

--- min-caml-hask/Closure.hs
+++ min-caml-hask/Closure.hs
@@ -6,12 +6,14 @@ import Data.Set (Set, difference, union)
 import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe (fromJust)
 import Debug.Trace
 
 import Id
 import Type
-import Syntax
-import KNormal hiding (freeVars)
+import Syntax hiding (name, args, body)
+import KNormal hiding (freeVars, name, args, body)
+import qualified KNormal
 -- | Type of closure.
 data Closure = Closure { entry :: LId, actualFV :: [VId] } deriving (Eq, Show)
 
@@ -37,9 +39,9 @@ data ClosExp
   | CExtArray !LId
   deriving (Eq, Show)
 data CFundef = CFundef
-    { name :: !(Id, Type)
-    , args :: ![(Id, Type)]
-    , formalFV :: ![(Id, Type)]
+    { name :: !(VId, Type)
+    , args :: ![(VId, Type)]
+    , formalFV :: ![(VId, Type)]
     , body :: !ClosExp
     } deriving (Eq, Show)
 
@@ -68,6 +70,9 @@ freeVars (CExtArray {}) = Set.empty
 idToVId :: Id -> VId
 idToVId (Id x) = VId x
 
+idToLId :: Id -> LId
+idToLId (Id x) = LId x
+
 transSub :: Map VId Type -> Set Id -> KNormal -> State [CFundef] ClosExp
 transSub env known e = case e of
   KUnit -> return CUnit
@@ -80,6 +85,36 @@ transSub env known e = case e of
   KIf cmp (Id x) (Id y) e1 e2 -> CIf cmp (VId x) (VId y) <$> transSub env known e1 <*> transSub env known e2
   KLet (Id x) t e1 e2 -> CLet (VId x) t <$> transSub env known e1 <*> transSub env known e2
   KVar (Id x) -> return $ CVar (VId x)
+  KLetRec (KFundef { KNormal.name = (x@(Id n), t), KNormal.args = yts, KNormal.body = e1 }) e2 -> -- 関数定義の場合
+      {- 関数定義let rec x y1 ... yn = e1 in e2の場合は、
+	 xに自由変数がない(closureを介さずdirectに呼び出せる)
+	 と仮定し、knownに追加してe1をクロージャ変換してみる -}
+    do
+      toplevel_backup <- get
+      let env' = Map.insert (VId n) t env
+      let known' = Set.insert x known
+      let newenv = (Map.fromList (map (\(Id y, z) -> (VId y, z)) yts) `Map.union` env')
+      e1' <- transSub newenv known' e1 
+      {- 本当に自由変数がなかったか、変換結果e1'を確認する
+       注意: e1'にx自身が変数として出現する場合はclosureが必要!
+         (thanks to nuevo-namasute and azounoman; test/cls-bug2.ml参照) -}
+      let zs = freeVars e1' `difference` Set.fromList (map (idToVId . fst) yts)
+      (known', e1') <-
+        if Set.null zs then return (known', e1') else do
+        {- 駄目だったら状態(toplevelの値)を戻して、クロージャ変換をやり直す -}
+        trace ("free variable(s) " ++ show zs ++ " found in function " ++ show x ++ "@.") $
+         trace ("function " ++ show x ++ " cannot be directly applied in fact@.") $ return ()
+        put toplevel_backup
+        e1' <- transSub newenv known e1
+        return (known, e1')
+      let zs = Set.toList (freeVars e1' `difference` (Set.map idToVId (Set.insert x (Set.fromList (map fst yts)))))  -- 自由変数のリスト
+      let zts = map (\z -> (z, fromJust (Map.lookup z env'))) zs -- ここで自由変数zの型を引くために引数envが必要
+      modify (CFundef { name = (idToVId x, t), args = map (\(Id y, z) -> (VId y, z)) yts, formalFV = zts, body = e1' } :) -- トップレベル関数を追加
+      e2' <- transSub env' known' e2
+      if Set.member (idToVId x) (freeVars e2') then -- xが変数としてe2'に出現するか
+        return $ CMakeCls (idToVId x) t (Closure { entry = idToLId x, actualFV = zs }) e2' -- 出現していたら削除しない
+      else
+	return (trace ("eliminating closure(s) " ++ show x ++ "@.") e2') -- 出現しなければMakeClsを削除
   KApp (Id x) ys | Set.member (Id x) known -> -- 関数適用の場合
       return $ trace ("directly applying " ++ x ++ "@.") (CAppDir (LId x) (map idToVId ys))
   KApp (Id f) xs -> return $ CAppCls (VId f) (map idToVId xs)
