67a4136dae09c4105401a6230c3622b4a9c40f77
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2014-11-09 10:47:58 +0900

    Update .travis-ci.sh
    travis performs raytracing

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,4 +2,8 @@ sudo apt-get update -qq
 sudo apt-get install ocaml omake
 git submodule update --init --recursive
 make
+build/compiler raytracer/min-rt -glib raytracer/globals -lib min-caml/zebius/libmincaml.txt >/dev/null
+build/assembler raytracer/min-rt.s >/dev/null
+mv raytracer/min-rt raytracer/min-rt.x
+make -C ray-run/ contest.run
 
